ed1979efd462bd9fbab3d21f7cddfe0bd25aa990
vinaykrishnanelly <vnelly@hawk.iit.edu>
2024-02-26 18:07:49 -0800

    editing about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,15 +13,10 @@ export default class About extends Component {
                                         <div className="about-desc">
                                             <span className="heading-meta">About Us</span>
                                             <h2 className="colorlib-heading">Who Am I?</h2>
-                                            <p><strong>Hi Vinay Krishna Nelly</strong> On her way she met a copy. The copy
-                                                warned the Little Blind Text, that where it came from it would have been
-                                                rewritten a thousand times and everything that was left from its origin
-                                                would be the word "and" and the Little Blind Text should turn around and
-                                                return to its own, safe country.</p>
-                                            <p>Even the all-powerful Pointing has no control about the blind texts it is
-                                                an almost unorthographic life One day however a small line of blind text
-                                                by the name of Lorem Ipsum decided to leave for the far World of
-                                                Grammar.</p>
+                                            <p><strong>Hi Vinay Krishna Nelly</strong> a software engineer who loves solving problems and making things work better.
+                                                I've worked at some cool places like Amazon Web Services and Nordstrom.
+                                                What I enjoy the most is creating software that makes a difference, whether it's making systems run faster,
+                                                saving money, or just making users' lives easier.</p>
                                         </div>
                                     </div>
                                 </div>
