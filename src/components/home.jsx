23b556756a22dbfbed4f1261b54c5d32acb66bfc
vinaykrishnanelly <vnelly@hawk.iit.edu>
2024-02-26 17:44:03 -0800

    editing home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -16,11 +16,8 @@ export default class Home extends Component {
                                             <div className="slider-text-inner js-fullheight">
                                                 <div className="desc">
                                                     <h1>Hi! <br/>I'm Vinay</h1>
-                                                    <h2>100% html5 bootstrap templates Made by <a
-                                                        href="https://colorlib.com/" target="_blank">colorlib.com</a>
-                                                    </h2>
                                                     <p><a className="btn btn-primary btn-learn">Download CV <i
-                                                        className="icon-download4"/></a></p>
+                                                        className="icon-download4"/></a>https://drive.google.com/file/d/1lgdNQxYQ69SkpA8F1HjceKdUaWkAi9wc/view</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -35,10 +32,7 @@ export default class Home extends Component {
                                             className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                             <div className="slider-text-inner">
                                                 <div className="desc">
-                                                    <h1>I am <br/>a Designer</h1>
-                                                    <h2>100% html5 bootstrap templates Made by <a
-                                                        href="https://colorlib.com/" target="_blank">colorlib.com</a>
-                                                    </h2>
+                                                    <h1>I am <br/>a Software Engineer</h1>
                                                     <p><a className="btn btn-primary btn-learn">View Portfolio <i
                                                         className="icon-briefcase3"/></a></p>
                                                 </div>
